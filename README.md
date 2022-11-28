3083f39b19b239c3105f2dd0379504208cbb706a
Luciano Holanda Porto <lug.dba@gmail.com>
2022-11-28 17:07:52 -0300

    criação de projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# API REST com banco de dados usando Java e Spring Boot
---
+++ src/main/java/br/eti/lug/userdept/UserdeptApplication.java
@@ -0,0 +1,13 @@
+package br.eti.lug.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdeptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserdeptApplication.class, args);
+    }
+
+}
